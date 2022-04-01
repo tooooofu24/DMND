010643ef2e6551e5e6cd9f56379f0adc94cd45ac
toya <toya24xxx@i.softbank.jp>
2022-04-01 21:59:38 +0900

    OfferのGETに能を追加込み機能を追加

--- app/UseCases/Offer/IndexAction.php
+++ app/UseCases/Offer/IndexAction.php
@@ -14,6 +14,28 @@ class IndexAction
             ->with(['buyer', 'seller', 'category.baseCategory', 'images'])
             ->withCount(['favorites']);
 
+        // 文字列検索
+        if ($request->q) {
+            $query = $query->where('title', 'like', "%{$request->q}%");
+        }
+
+        // カテゴリー検索
+        if ($request->category_id_list) {
+            foreach ($request->category_id_list as $category_id) {
+                $query = $query->where('category_id', $category_id);
+            }
+        }
+
+        // 公開中検索
+        if ($request->is_open) {
+            $query = $query->where('status', 0);
+        }
+
+        // 商品の状態検索
+        if ($request->condition) {
+            $query = $query->where('condition', $request->condition);
+        }
+
         return OfferResource::collection($query->paginate());
     }
 }
--- database/factories/OfferFactory.php
+++ database/factories/OfferFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Consts\Condition;
 use App\Consts\Pref;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -19,12 +20,12 @@ class OfferFactory extends Factory
             'buyer_id' => $this->faker->numberBetween(1, 20),
             'seller_id' => $this->faker->numberBetween(1, 20),
             'category_id' => $this->faker->numberBetween(1, 40),
-            'condition' => $this->faker->numberBetween(1, 4),
+            'condition' => $this->faker->randomElement(Condition::LIST),
             'received_pref' => $this->faker->randomElement(Pref::LIST),
             'delivery_fee_payment_burden' => $this->faker->numberBetween(0, 1),
             'payment_method' => $this->faker->numberBetween(1, 4),
             'price' => $this->faker->randomElement([1000, 2000, 3000, 4000, 5000, 10000]),
-            'status' => $this->faker->numberBetween(1, 4),
+            'status' => $this->faker->numberBetween(0, 4),
             'description' => $this->faker->realText(),
         ];
     }
