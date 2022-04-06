4fbd322467884443c538aa51e746f67e60aa65ab
toya <toya24xxx@i.softbank.jp>
2022-04-06 17:50:49 +0900

    offer_status_logsに名前変更

--- database/migrations/2022_03_30_181145_create_offer_status_logs_table.php
+++ database/migrations/2022_03_30_181145_create_offer_status_logs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOfferStatusHistoriesTable extends Migration
+class CreateOfferStatusLogsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateOfferStatusHistoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('offer_status_histories', function (Blueprint $table) {
+        Schema::create('offer_status_logs', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('offer_id');
             $table->tinyInteger('type')->comment('20 買い手がDEAL承諾した, 30 買い手が支払いした, 40 売り手が発送通知した, 50 買い手が受取通知した');
@@ -28,6 +28,6 @@ class CreateOfferStatusHistoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('offer_status_histories');
+        Schema::dropIfExists('offer_status_logs');
     }
 }
